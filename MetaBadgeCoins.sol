7dd84c1ab5d2115ca9b15ffd204a2ae8470f1a18
Adan Hernandez <101598731+cikejoae@users.noreply.github.com>
2022-06-23 01:39:10 -0700

    Update MetaBadgeCoins.sol

--- MetaBadgeCoins.sol
+++ MetaBadgeCoins.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-import "@openzeppelin/contracts@4.6.0/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts@4.6.0/token/ERC20/extensions/ERC20Burnable.sol";
-import "@openzeppelin/contracts@4.6.0/access/Ownable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
 /// @custom:security-contact security@osis.world
 contract MetaBadgeCoins is ERC20, ERC20Burnable, Ownable {
